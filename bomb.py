6c62d7408ec970d51f76e363b581fe6dfc4c16d7
glapsvidr <56077973+glapsvidr@users.noreply.github.com>
2020-09-22 11:54:41 +0000

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -29,9 +29,27 @@ data = load_data(1000)
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Done! (using st.cache)")
 
-if st.sidebar.checkbox('Show raw data'):
-    st.sidebar.subheader('Raw data')
-    st.sidebar.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+#'Starting to load data'	
+
+## Add a placeholder
+#latest_iteration = st.empty()
+#bar = st.progress(0)
+
+#for i in range(100):
+#  # Update the progress bar with each iteration.
+#  latest_iteration.text(f'Iteration {i+1}')
+#  bar.progress(i + 1)
+#  time.sleep(0.1)
+
+#'...and now we\'re done!'
+
+data['latitude'] = data['latitude'].dropna()
+data['longitude'] = data['longitude'].dropna()
+
 #############################
 #sidebar
 
@@ -40,23 +58,45 @@ Airforce = st.sidebar.multiselect('Airforce', data['country_flying_mission'].uni
 
 theater = st.sidebar.multiselect('theater', data['theater'].unique())
 
+start_date = st.sidebar.date_input('start date', datetime.date(1939,1,1))
+end_date = st.sidebar.date_input('end date', datetime.date(1949,1,1))
+
+
+###
+
+#mask = (data['msndate'] > '1939-1-1') & (data['msndate'] <= '1949-1-1')
+#st.write(data.loc[mask])
 
 ##########
 
 # Filter dataframe
+
+data['latitude'] = data['latitude'].dropna()
+data['longitude'] = data['longitude'].dropna()
+
 new_df = data[(data['country_flying_mission'].isin(Airforce)) & (data['theater'].isin(theater))]
 
 
+
 # write dataframe to screen
-st.write(new_df)
+#st.write(new_df)
 
 
-new_df['latitude'] = data['latitude'].dropna().apply(np.int64)
-new_df['longitude'] = data['longitude'].dropna().apply(np.int64)
 
 
-st.write("the latitude type is:")
-new_df['latitude'].dtype
 
 
-st.map(data)
+#####################
+st.map(new_df)
+
+
+
+
+######
+start_color, end_color = st.select_slider(
+    'Select a range of color wavelength',
+    options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
+    value=('red', 'blue'))
+st.write('You selected wavelengths between', start_color, 'and', end_color)
+
+
